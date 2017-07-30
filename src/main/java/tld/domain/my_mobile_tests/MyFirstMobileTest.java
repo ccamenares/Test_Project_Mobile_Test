d44e56af6bc453224ac713ccfa6e8b2be6cac950
Christopher <camenarescoding@gmail.com>
2017-07-30 19:26:26 -0400

    Create AndroidDriver object

--- src/main/java/tld/domain/my_mobile_tests/MyFirstMobileTest.java
+++ src/main/java/tld/domain/my_mobile_tests/MyFirstMobileTest.java
@@ -1,5 +1,7 @@
 package tld.domain.my_mobile_tests;
 
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
 import io.testproject.java.enums.ExecutionResultType;
 import io.testproject.java.sdk.generated.codeblocks.Test;
 
@@ -8,6 +10,10 @@ public class MyFirstMobileTest extends Test {
 	@Override
 	protected ExecutionResultType execute() throws Exception {
 		// TODO Auto-generated method stub
+		
+		// Get driver initialized by TestProject Agent
+		AndroidDriver<MobileElement> driver = this.getAndroidDriver(MobileElement.class);
+		
 		return null;
 	}
 
