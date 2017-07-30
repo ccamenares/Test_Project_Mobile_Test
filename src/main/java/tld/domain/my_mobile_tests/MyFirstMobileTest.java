ed84e41cc7f6ccfede477c3d4496c3c18deee00a
Christopher <camenarescoding@gmail.com>
2017-07-30 19:22:53 -0400

    Create Test class

---
+++ src/main/java/tld/domain/my_mobile_tests/MyFirstMobileTest.java
@@ -0,0 +1,14 @@
+package tld.domain.my_mobile_tests;
+
+import io.testproject.java.enums.ExecutionResultType;
+import io.testproject.java.sdk.generated.codeblocks.Test;
+
+public class MyFirstMobileTest extends Test {
+
+	@Override
+	protected ExecutionResultType execute() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
