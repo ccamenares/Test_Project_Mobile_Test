134fdd3db896ddd04aeed541d065b0d7df9ea85c
Christopher <camenarescoding@gmail.com>
2017-07-30 21:51:49 -0400

    Add a developer token

--- src/main/java/tld/domain/my_mobile_tests/MyFirstMobileTest_Runner.java
+++ src/main/java/tld/domain/my_mobile_tests/MyFirstMobileTest_Runner.java
@@ -1,5 +1,8 @@
 package tld.domain.my_mobile_tests;
 
 public class MyFirstMobileTest_Runner {
+	
+	// Setting the development token
+	private final static String devToken = "YOUR_DEV_TOKEN_GOES_HERE";
 
 }
