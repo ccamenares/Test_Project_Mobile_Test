c37bf1dff7471ea84e53972d0c79545ebfee489c
Christopher <camenarescoding@gmail.com>
2017-07-30 20:42:47 -0400

    Validate Full Name

--- src/main/java/tld/domain/my_mobile_tests/MyFirstMobileTest.java
+++ src/main/java/tld/domain/my_mobile_tests/MyFirstMobileTest.java
@@ -22,7 +22,22 @@ public class MyFirstMobileTest extends Test {
 		// Find Last name element and type 'Smith'
 		MobileElement lastName = driver.findElement(By.id("lastName")); lastName.sendKeys("Smith");
 		
-		return null;
+		// Find Full Name element
+		MobileElement fullName = driver.findElement(By.id("fullName"));
+		
+		
+		
+		// Verify that Full Name equals a concatenation of First and Last 
+		String fullNameText = fullName.getText();
+		if (fullNameText.equals("John Smith")) { 
+			this.setMessage("Full name as expected"); 
+			return ExecutionResultType.Passed;
+		} 
+		else {
+			this.setMessage("Full name = " + fullNameText); 
+			return ExecutionResultType.Failed;
+		}
+		
 	}
 
 }
