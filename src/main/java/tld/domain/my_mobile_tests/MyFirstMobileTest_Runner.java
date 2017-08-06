25890e4f82539ab9784ad4b80386139cbe66e3f9
Christopher <camenarescoding@gmail.com>
2017-08-06 16:39:06 -0400

    Get driver settings

--- src/main/java/tld/domain/my_mobile_tests/MyFirstMobileTest_Runner.java
+++ src/main/java/tld/domain/my_mobile_tests/MyFirstMobileTest_Runner.java
@@ -1,8 +1,36 @@
 package tld.domain.my_mobile_tests;
 
+import io.testproject.java.classes.DriverSettings;
+import io.testproject.java.classes.MobileDriverSettings;
+import io.testproject.java.enums.DriverType;
+import io.testproject.java.sdk.generated.TestProjectRunner;
+
 public class MyFirstMobileTest_Runner {
 	
 	// Setting the development token
 	private final static String devToken = "YOUR_DEV_TOKEN_GOES_HERE";
 
+	// Initializes driver settings
+	private static DriverSettings getDriverSettings() {
+		DriverSettings driverSettings = new DriverSettings();
+
+		// Declare that Android driver is required
+		driverSettings.setDriverType(DriverType.Appium_Android);
+
+		// Specify target device UDID and Name
+		// This information is conveniently visible at https://app.testproject.io/#/agents
+		// Under your connected agent on the devices tab
+		driverSettings.setDeviceUdid("DEVICE_UDID");
+		driverSettings.setDeviceName("DEVICE_NAME");
+
+		// Specify mobile app settings
+		MobileDriverSettings mobileSettings = new MobileDriverSettings();
+		mobileSettings.setAndroidPackage("io.testproject.demo");
+		mobileSettings.setAndroidActivity(".MainActivity");
+		driverSettings.setMobileSettings(mobileSettings);
+
+		return driverSettings;
+	}
+
+	
 }
